6ebda2321b64431a08de20392ba90dd25daee3ea
Pete Dubler <pete.dubler@gmail.com>
2024-01-19 09:40:17 -0500

    first commit

---
+++ CYD_SD_BREAKOUT_DEMO_events.py
@@ -0,0 +1,28 @@
+
+def Button1PressedAction(event_struct):
+    return
+
+
+def FunctionToggleGPIO18(event_struct):
+    return
+
+
+def FunctionToggleGPIO19(event_struct):
+    return
+
+
+def FunctionToggleGPIO5(event_struct):
+    return
+
+
+def FunctionToggleGPIO23(event_struct):
+    return
+
+
+def ReturnClickedClearStates(event_struct):
+    return
+
+
+def ReturnClickedRestoreStates(event_struct):
+    return
+
---
+++ src/settings.h
@@ -0,0 +1,14 @@
+#ifndef _SETTINGS_H_
+#define _SETTINGS_H_
+
+#define LED_ON 0  // GPIOs go low to light the LEDs
+#define LED_OFF 1
+
+#define GPIO5 5
+#define GPIO18 18
+#define GPIO19 19
+#define GPIO23 23
+
+//#define AddSerialOutput
+
+#endif
